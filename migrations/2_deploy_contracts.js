29dcf3db45a84bf8075b0323a35bebe7ba5138f1
eoinwoods <eoin.woods@artechra.com>
2017-06-25 21:42:06 +0100

    Add external script for demo purposes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,8 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var SpaCoin = artifacts.require("./spacoin.sol");
 
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(SpaCoin);
 };
---
+++ scripts/call_spa_coin.js
@@ -0,0 +1,24 @@
+'use strict';
+var SpaCoin = artifacts.require("SpaCoin");
+
+module.exports = function(exitCallback) {
+	var coin ;
+
+	var CONTRACT_ADDR = "0x16bc4143116ecafed7168bdf4169573982da0a15" ;
+
+	var testuser = web3.eth.accounts[1] ;
+
+	// or SpaCoin.deployed().then(...) to find it
+	SpaCoin.at(CONTRACT_ADDR).then(function(instance){
+		coin = instance ;
+		return coin.outstandingValue.call() ;
+	}).then(function(result) {
+		console.log("Network has " + result.toNumber() + " coins outstanding") ;
+		return coin.allocate(testuser, 1000) ;
+	}).then(function(result){
+		console.log("Allocation transaction hash is: " + result.tx) ;
+		return coin.holderBalance.call(testuser) ;
+	}).then(function(result) {
+		console.log("Test user " + testuser + " has " + result.toNumber() + " coins after allocation") ;
+	});
+}
